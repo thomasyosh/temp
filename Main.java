dd6b66829ee948509946d2c35363acfe4c3adf91
John Doe <johndoe@email.com>
2023-11-10 17:38:39 +0800

    sampleref

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 class Main{
     static Queue<Integer> q = new LinkedList<Integer>();
+
+    static String currentTaskName = "";
     static Scanner sc = new Scanner(System.in);
+    //static String [] service = {"New Mem", "Gift", "Check Bal", "Trans Bal"};
+    static String x = "[";
 
     static void promptMsg(){
         int serviceCode = -1;
@@ -15,23 +17,27 @@ class Main{
             if (serviceCode>4)
                 throw new ArrayIndexOutOfBoundsException();
             
+            
             switch (serviceCode){
                 case 1:
-                    for (int i=0;i<4;i++){
-                        q.add(serviceCode);
-                    }                    
+                    for (int i=3;i>=0;i--){
+                        q.add(i);
+                    }
+                    x += "(4 new member) ";
                 break;
                 case 2:
-                for(int i=0;i<2;i++){
-                    q.add(serviceCode);
+                for(int i=1;i>=0;i--){
+                    q.add(i);
                 }
+                    x += "(2 gift) ";
                 break;
                 case 3:
-                    q.add(serviceCode);                   
+                    q.add(0);
+                    x += "(1 check bal) ";                
                 break;
                 case 4:
-                for(int i=0;i<3;i++){
-                    q.add(serviceCode);
+                for(int i=2;i>=0;i--){
+                    q.add(i);
                 }
                 break;
             }                   
@@ -40,23 +46,24 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Input simulation time (mins):");
         int minutes = Integer.parseInt(sc.nextLine());
+
+
+
         System.out.println("---------------- START OF SIMULATION -----------------");
     
         for (int i=1;i<minutes+1;i++){
             promptMsg();
-            System.out.print("After "+i+" min(s)");
             q.poll();
-            System.out.print(" "+q+"\n");
+            if (q.element()!=0){
+                x.replaceFirst("[0-9]","3");
+            }
+
+            System.out.print("After "+i+" min(s)");
+            System.out.println(q+"\n");
+            System.out.println(x);
             System.out.println("------------------------------------------------------");
         }
-        
-
-
-
-
-        
-        
-        
+ 
     }
     
 
